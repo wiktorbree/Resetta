1841268240c93ab95e71cd92dc0674244e54dcbe
Wiktor Bramer <bramerwiktor@gmail.com>
2026-05-15 22:11:29 +0200

    Initial Commit

---
+++ Resetta/ResettaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ResettaApp.swift
+//  Resetta
+//
+//  Created by Wiktor on 15/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct ResettaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
