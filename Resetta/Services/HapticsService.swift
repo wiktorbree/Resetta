3917ea8c8055bfebc50ded92079ecc3cb7daf459
Wiktor Bramer <bramerwiktor@gmail.com>
2026-05-17 13:55:02 +0200

    Tune session haptics to be more perceptible

--- Resetta/Services/HapticsService.swift
+++ Resetta/Services/HapticsService.swift
@@ -1,16 +1,17 @@
 import UIKit
 
+@MainActor
 enum HapticsService {
     static func sessionStarted(enabled: Bool) {
-        subtleImpact(enabled: enabled, intensity: 0.32)
+        subtleImpact(enabled: enabled, intensity: 0.46)
     }
 
     static func endSessionButtonRevealed(enabled: Bool) {
-        subtleImpact(enabled: enabled, intensity: 0.26)
+        subtleImpact(enabled: enabled, intensity: 0.42)
     }
 
     static func sessionCompleted(enabled: Bool) {
-        subtleImpact(enabled: enabled, intensity: 0.38)
+        subtleImpact(enabled: enabled, intensity: 0.54)
     }
 
     static func lightImpact(enabled: Bool) {
@@ -30,7 +31,7 @@ enum HapticsService {
 
     private static func subtleImpact(enabled: Bool, intensity: CGFloat) {
         guard enabled else { return }
-        let generator = UIImpactFeedbackGenerator(style: .soft)
+        let generator = UIImpactFeedbackGenerator(style: .light)
         generator.prepare()
         generator.impactOccurred(intensity: intensity)
     }
